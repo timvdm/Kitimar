31ecccf4b8057255eb31ad19821462322f14dc52
Tim Vandermeersch <tim.vandermeersch@gmail.com>
2023-06-03 01:45:41 +0200

    Workaround missing std::ranges::owning_view in GCC 11

--- src/Kitimar/CTSmarts/CTSmarts.hpp
+++ src/Kitimar/CTSmarts/CTSmarts.hpp
@@ -4,41 +4,63 @@
 
 namespace Kitimar::CTSmarts {
 
-    template<typename Molecule, typename Smarts, auto N>
-    auto captureAtoms(Molecule &mol, Smarts, const IsomorphismMapping &map, const std::array<int, N> &cap)
-    {
-        using Atom = decltype(get_atom(mol, 0));
-        if constexpr (N) {
-            std::array<Atom, N> atoms = {};
-            if (map.empty())
-                atoms.fill(null_atom(mol));
-            else
-                for (auto i = 0; i < N; ++i)
-                    atoms[i] = get_atom(mol, map[cap[i]]);
-            return atoms;
-        } else {
-            std::array<Atom, Smarts::numAtoms> atoms = {};
-            if (map.empty())
-                atoms.fill(null_atom(mol));
-            else
-                for (auto i = 0; i < Smarts::numAtoms; ++i)
-                    atoms[i] = get_atom(mol, map[i]); // FIXME: null atoms...
-            return atoms;
+    namespace detail {
+
+        template<typename Molecule, typename Smarts, auto N>
+        auto captureAtoms(Molecule &mol, Smarts, const IsomorphismMapping &map, const std::array<int, N> &cap)
+        {
+            using Atom = decltype(get_atom(mol, 0));
+            if constexpr (N) {
+                std::array<Atom, N> atoms = {};
+                std::cout << "atoms.size(): " << atoms.size() << std::endl;
+                std::cout << "map.size(): " << map.size() << std::endl;
+                std::cout << "cap.size(): " << cap.size() << std::endl;
+                if (map.empty())
+                    atoms.fill(null_atom(mol));
+                else
+                    for (auto i = 0; i < N; ++i) {
+                        std::cout << "i: " << i << std::endl;
+                        std::cout << "    cap[i]: " << cap[i] << std::endl;
+                        std::cout << "    map[cap[i]]: " << map[cap[i]] << std::endl;
+                        atoms[i] = get_atom(mol, map[cap[i]]);
+                    }
+                return atoms;
+            } else {
+                std::array<Atom, Smarts::numAtoms> atoms = {};
+                if (map.empty())
+                    atoms.fill(null_atom(mol));
+                else
+                    for (auto i = 0; i < Smarts::numAtoms; ++i)
+                        atoms[i] = get_atom(mol, map[i]); // FIXME: null atoms...
+                return atoms;
+            }
         }
-    }
 
 
-    template<typename Molecule, typename Smarts, auto N>
-    auto captureMatchAtoms(Molecule &mol, Smarts smarts, const IsomorphismMapping &map, const std::array<int, N> &cap)
-    {
-        return std::tuple_cat(std::make_tuple(!map.empty()), captureAtoms(mol, smarts, map, cap));
-    }
+        template<typename Molecule, typename Smarts, auto N>
+        auto captureMatchAtoms(Molecule &mol, Smarts smarts, const IsomorphismMapping &map, const std::array<int, N> &cap)
+        {
+            return std::tuple_cat(std::make_tuple(!map.empty()), captureAtoms(mol, smarts, map, cap));
+        }
 
+        template<auto N>
+        auto copyCapture(Molecule::Molecule auto &mol, const auto &iso, const std::array<int, N> &cap, const auto &caps) noexcept
+        {
+            using Atom = decltype(get_atom(mol, 0));
+            static constexpr auto M = N ? N : iso.smarts.numAtoms;
+            std::vector<std::array<Atom, M>> v;
+            auto r = caps | std::views::transform([&] (const auto &map) {
+                return captureAtoms(mol, iso.smarts, map, cap);
+            });
+            std::ranges::copy(r, std::back_inserter(v));
+            return v;
+        }
+
+    } // namespace detail
 
     //
     // Molecule
     //
-
 
     //
     // ctsmarts::match<SMARTS>(mol) -> bool
@@ -51,7 +73,7 @@ namespace Kitimar::CTSmarts {
     }
 
     //
-    // ctsmarts::single<SMARTS>(mol) -> Mapping
+    // ctsmarts::single<SMARTS>(mol) -> std::vector<int>
     //
     template<ctll::fixed_string SMARTS, typename Molecule>
     constexpr auto single(Molecule &mol)
@@ -61,7 +83,7 @@ namespace Kitimar::CTSmarts {
     }
 
     //
-    // ctsmarts::count<SMARTS>(mol) -> integer
+    // ctsmarts::count<SMARTS>(mol) -> std::integeral
     //
     template<ctll::fixed_string SMARTS, typename Molecule>
     constexpr auto count(Molecule &mol)
@@ -71,7 +93,7 @@ namespace Kitimar::CTSmarts {
     }
 
     //
-    // ctsmarts::unique<SMARTS>(mol) -> list<Mapping>
+    // ctsmarts::unique<SMARTS>(mol) -> std::vector<std::vector<int>>
     //
     template<ctll::fixed_string SMARTS, typename Molecule>
     constexpr auto unique(Molecule &mol)
@@ -81,7 +103,7 @@ namespace Kitimar::CTSmarts {
     }
 
     //
-    // ctsmarts::countAll<SMARTS>(mol) -> integer
+    // ctsmarts::countAll<SMARTS>(mol) -> std::integeral
     //
     template<ctll::fixed_string SMARTS, typename Molecule>
     constexpr auto countAll(Molecule &mol)
@@ -91,7 +113,7 @@ namespace Kitimar::CTSmarts {
     }
 
     //
-    // ctsmarts::all<SMARTS>(mol) -> list<Mapping>
+    // ctsmarts::all<SMARTS>(mol) -> std::vector<std::vector<int>>
     //
     template<ctll::fixed_string SMARTS, typename Molecule>
     constexpr auto all(Molecule &mol)
@@ -110,8 +132,7 @@ namespace Kitimar::CTSmarts {
         constexpr auto cap = captureMapping(smarts);
         auto iso = SingleIsomorphism<SMARTS>{};
         auto map = iso.single(mol);
-
-        return captureMatchAtoms(mol, smarts, map, cap);
+        return detail::captureMatchAtoms(mol, smarts, map, cap);
     }
 
     template<MapType T>
@@ -121,16 +142,20 @@ namespace Kitimar::CTSmarts {
     static constexpr auto CaptureAll = CaptureTag<MapType::All>{};
 
     //
-    // ctsmarts::captures<SMARTS>(mol) -> std::range<std::tuple<bool, Atom...>>
+    // ctsmarts::captures<SMARTS>(mol) -> std::range<std::array<Atom...>>
     //
     template <ctll::fixed_string SMARTS, MapType CaptureType = MapType::Unique>
     auto captures(Molecule::Molecule auto &mol, CaptureTag<CaptureType> = {})
     {
         auto iso = Isomorphism<SMARTS, CaptureType>{};
-        constexpr auto cap = captureMapping(iso.smarts);
-        return iso.all(mol) | std::views::transform([&] (const auto &map) {
-            return captureAtoms(mol, iso.smarts, map, cap);
-        });
+        static constexpr auto cap = captureMapping(iso.smarts);
+        if constexpr (__cpp_lib_ranges >= 202110L)
+            return iso.all(mol) | std::views::transform([&] (const auto &map) {
+                return detail::captureAtoms(mol, iso.smarts, map, cap);
+            });
+        else
+            // missing std::ranges::owning_view
+            return detail::copyCapture(mol, iso, cap, iso.all(mol));
     }
 
 
@@ -138,76 +163,21 @@ namespace Kitimar::CTSmarts {
     // Atom
     //
 
-
     //
-    // ctsmarts::captures<SMARTS>(mol, atom) -> std::range<std::tuple<bool, Atom...>>
+    // ctsmarts::captures<SMARTS>(mol, atom) -> std::range<std::array<Atom...>>
     //
     template <ctll::fixed_string SMARTS, MapType CaptureType = MapType::Unique>
     auto captures(Molecule::Molecule auto &mol, const auto &atom, CaptureTag<CaptureType> = {})
     {
         auto iso = Isomorphism<SMARTS, CaptureType>{};
-        constexpr auto cap = captureMapping(iso.smarts);
-        return iso.all(mol, atom) | std::views::transform([&] (const auto &map) {
-            return captureAtoms(mol, iso.smarts, map, cap);
-        });
+        static constexpr auto cap = captureMapping(iso.smarts);
+        if constexpr (__cpp_lib_ranges >= 202110L)
+            return iso.all(mol, atom) | std::views::transform([&] (const auto &map) {
+                return detail::captureAtoms(mol, iso.smarts, map, cap);
+            });
+        else
+            // missing std::ranges::owning_view
+            return detail::copyCapture(mol, iso, cap, iso.all(mol, atom));
     }
-
-
-    //
-    // API
-    //
-
-    /*
-
-
-    Molecule mol;
-
-
-    ctsmarts::match  <SMARTS> (mol)         -> boolean
-    ctsmarts::count  <SMARTS> (mol, unique) -> integer
-    ctsmarts::single <SMARTS> (mol)         -> mapping
-    ctsmarts::unique <SMARTS> (mol)         -> list<mapping>
-    ctsmarts::all    <SMARTS> (mol)         -> list<mapping>
-
-
-    ctsmarts::match  <SMARTS> ([mol,] atom)          -> boolean
-    ctsmarts::count  <SMARTS> ([mol,] atom, unique)  -> integer
-    ctsmarts::single <SMARTS> ([mol,] atom)          -> mapping
-    ctsmarts::unique <SMARTS> ([mol,] atom)          -> list<mapping>
-    ctsmarts::all    <SMARTS> ([mol,] atom)          -> list<mapping>
-
-    ctsmarts::capture  <SMARTS>(mol)                 -> tuple<boolean, atom...>
-    ctsmarts::captures <SMARTS>(mol, unique)         -> list<tuple<boolean, atom...>>
-
-    ctsmarts::capture  <SMARTS>([mol,] atom)         -> tuple<boolean, atom...>
-    ctsmarts::captures <SMARTS>([mol,] atom, unique) -> list<tuple<boolean, atom...>>
-
-
-    // FIXME captureAll captureAll
-
-
-    auto [match, a, b] = ctsmarts<"[*:1]=[:2]">::capture(mol) -> tuple(boolean, atom...)
-
-    auto [match, a, b] = ctsmarts<"[*:1]=[:2]">::capture(mol, atom) -> tuple(boolean, atom...)
-
-
-    for (auto [a, b] : ctsmarts<"[*:1]=[:2]">::captures(mol, atom))
-
-
-    */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 } // namespace Kitimar::CTSmarts
